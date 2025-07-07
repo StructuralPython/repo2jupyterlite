abc80c8653340b55eb78e0921c640a1cc1b2194a
Connor Ferster <connorferster@gmail.com>
2025-07-07 10:10:31 -0700

    added jupyterlab_myst to required packages

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     install_requires=[
         "jupyterlite-core[all]",
         "jupyterlite-pyodide-kernel",
+        "jupyterlab_myst",
         "jupyter-repo2docker",
         "yarl",
     ],
